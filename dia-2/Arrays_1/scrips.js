e06c8d9bc50a5c25f7d8de02e9e8875b218c5cc2
DaniloBertolini <danilocbertolini@gmail.com>
2023-03-29 15:05:12 -0300

    .

---
+++ dia-2/Arrays_1/scrips.js
@@ -0,0 +1,27 @@
+/*
+
+.length - Mostra o tamanho
+.unshift - coloca no começo
+.push - coloca no final
+.shift - tira o primeiro
+.pop - tira o ultimo
+.sort - ordem alfabetica
+.indexOf
+
+*/
+
+let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//
+
+let indexOfPortfolio = menu.indexOf('Portifólio');
+
+console.log(indexOfPortfolio);
+
+//
+
+let final = menu.push('Contato')
+console.log(menu);
