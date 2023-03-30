2a6a920dbbc4356af029327efa573fc06c6f0ba3
DaniloBertolini <danilocbertolini@gmail.com>
2023-03-30 12:44:03 -0300

    .

--- dia-2/Exercicios_5/scipts2.js
+++ dia-2/Exercicios_5/scipts2.js
@@ -1,10 +1,37 @@
-numero = 5;
+numero = 6;
 let resultado = numero;
 
-for (let a=numero; a > 0; a -= 1) {
+for (let a=numero; a > 1; a -= 1) {
     resultado = resultado * (a-1)
 }
 console.log(resultado)
 
 //
 
+let palavra = 'dinhoOvirtuoso';
+let novapalavra = "";
+for (let b = palavra.length-1; b >= 0; b -= 1) {
+    novapalavra += palavra[b]
+}
+
+console.log(novapalavra)
+
+//
+
+let array = ['java', 'javascript', 'python', 'html', 'css']
+
+/* for (); */
+
+let maiorpalavra = '';
+let menorpalavra = array[0];
+for (let c=0; c < array.length; c += 1) {
+    if (maiorpalavra.length < array[c].length) {
+        maiorpalavra = array[c];
+    }
+    if (menorpalavra.length > array[c].length) {
+        menorpalavra = array[c];
+    }
+}
+
+console.log(`A maior palavra é ${maiorpalavra} com ${maiorpalavra.length} letras`)
+console.log(`A menor palavra é ${menorpalavra} com ${menorpalavra.length} letras`)
