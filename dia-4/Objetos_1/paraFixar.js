814fa0ac8bd7bbcf240375133a804ee3ac982d98
DaniloBertolini <danilocbertolini@gmail.com>
2023-04-03 15:16:32 -0300

    .

--- dia-4/Objetos_1/paraFixar.js
+++ dia-4/Objetos_1/paraFixar.js
@@ -1,4 +1,16 @@
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age:34,
+    medals: {
+        golden: 2, 
+        silver: 3
+        }
+}
+
+/* “A jogadora Marta Silva tem 34 anos de idade”. */
+console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade.`)
+
+
+//
+
