9b05a18f6e3280b5ec30d42889a628d62f2220cc
DaniloBertolini <danilocbertolini@gmail.com>
2023-04-12 11:22:14 -0300

    .

---
+++ 1_Fundamentos/secao-04-javascript-dom-eventos-web-storage/dia-1-javascript-dom-e-seletores/Qual_Seletor_2/scripts.js
@@ -0,0 +1,5 @@
+const header = document.getElementById('header-container');
+header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+const tasksEmergency = document.getElementsByClassName('emergency-tasks')[0];
+tasksEmergency.style.backgroundColor = 'rgb(255, 159, 132)';
