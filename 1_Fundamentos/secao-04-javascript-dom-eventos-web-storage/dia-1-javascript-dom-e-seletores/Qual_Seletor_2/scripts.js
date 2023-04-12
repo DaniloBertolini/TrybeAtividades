734ecbb6c0e17befee6c68a8860ecb3240e63265
DaniloBertolini <danilocbertolini@gmail.com>
2023-04-12 12:51:17 -0300

    .

--- 1_Fundamentos/secao-04-javascript-dom-eventos-web-storage/dia-1-javascript-dom-e-seletores/Qual_Seletor_2/scripts.js
+++ 1_Fundamentos/secao-04-javascript-dom-eventos-web-storage/dia-1-javascript-dom-e-seletores/Qual_Seletor_2/scripts.js
@@ -3,3 +3,19 @@ header.style.backgroundColor = 'rgb(0, 176, 105)';
 
 const tasksEmergency = document.getElementsByClassName('emergency-tasks')[0];
 tasksEmergency.style.backgroundColor = 'rgb(255, 159, 132)';
+
+const tasksEmergencyHeader = document.querySelectorAll('.emergency-tasks h3');
+for (let i=0; i<tasksEmergencyHeader.length; i+= 1) {
+    tasksEmergencyHeader[i].style.backgroundColor = 'rgb(165, 0, 243)'
+}
+
+const tasksNoEmergency = document.getElementsByClassName('no-emergency-tasks')[0];
+tasksNoEmergency.style.backgroundColor = 'rgb(249, 219, 94)';
+
+const tasksNoEmergencyHeader = document.querySelectorAll('.no-emergency-tasks h3');
+for (let i=0; i<tasksNoEmergencyHeader.length; i+= 1) {
+    tasksNoEmergencyHeader[i].style.backgroundColor = 'rgb(35, 37, 37)'
+}
+
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'rgb(0, 53, 51)';
